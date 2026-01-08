c50285a92e3317c112126589a5a4ecf6ce7be1c5
saikrishnagorijala12 <saikrishnagorijala14@gmail.com>
2026-01-08 15:29:28 +0530

    updated main.py and gitignore

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import uvicorn
-
 from app import create_app
-
+from fastapi.middleware.cors import CORSMiddleware
+ 
 app = create_app()
-
+ 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+ 
+ 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
