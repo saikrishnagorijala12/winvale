d44f84b129a1137b7fedc83356df30f84f2c58db
saikrishnagorijala12 <saikrishnagorijala14@gmail.com>
2026-01-27 13:44:21 +0530

    updated code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import uvicorn
+# import uvicorn
 from app import create_app
 from fastapi.middleware.cors import CORSMiddleware
  
@@ -6,12 +6,13 @@ app = create_app()
  
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    # allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
  
  
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", reload=True)
